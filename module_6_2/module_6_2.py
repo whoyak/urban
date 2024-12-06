4767a22fc4177b1f21feed1788486ef32f584d09
whoyak <73415603+whoyak@users.noreply.github.com>
2024-12-06 20:54:03 +0300

    Add files via upload

---
+++ module_6_2/module_6_2.py
@@ -0,0 +1,51 @@
+class Vehicle:
+    __COLOR_VARIANTS = ["Red", "Blue", "Green", "Black"]
+
+    def __init__(self,owner:str, model:str,engine_power:int,color:str):
+        self.owner = owner
+        self.__model = model
+        self.__engine_power = engine_power
+        self.__color = color
+
+
+    def get_model(self):
+        return f'Модель: {self.__model}'
+
+    def get_horsepower(self):
+        return f'Мощность двигателя: {self.__engine_power}'
+
+    def get_color(self):
+        return f'Цвет: {self.__color}'
+
+    def print_info(self):
+        print(self.get_model())
+        print(self.get_horsepower())
+        print(self.get_color())
+        print(f'Владелец: {self.owner}')
+
+    def set_color(self,new_color:str):
+        if new_color.lower() in [c.lower() for c in Vehicle.__COLOR_VARIANTS]:
+            self.color = new_color
+        else:
+            print(f'Нельзя сменить цвет на {new_color}')
+
+
+class Sedan(Vehicle):
+    __PASSENGERS_LIMIT = 5
+
+    def get_passengers(self):
+        return f' Ограничение по пасажирам: {Sedan.__PASSENGERS_LIMIT}'
+
+# Текущие цвета __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+vehicle1 = Sedan('Fedos', 'Toyota Mark II', 500 , 'blue')
+
+# Изначальные свойства
+vehicle1.print_info()
+
+# Меняем свойства (в т.ч. вызывая методы)
+vehicle1.set_color('Pink')
+vehicle1.set_color('BLACK')
+vehicle1.owner = 'Vasyok'
+
+# Проверяем что поменялось
+vehicle1.print_info()
