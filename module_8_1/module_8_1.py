65f5231f32ad359e2e0b108259eba8c71dfc6288
whoyak <73415603+whoyak@users.noreply.github.com>
2024-12-15 14:44:54 +0300

    Add files via upload

---
+++ module_8_1/module_8_1.py
@@ -0,0 +1,12 @@
+def add_everything_up(a:int|float|str,b:int|float|str):
+    try:
+        return a+b
+    except TypeError:
+        return f'{a}{b}'
+
+
+
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
