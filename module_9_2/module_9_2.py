be0e538e85113ae19e2515bd6c8c4cc6d7836a0b
whoyak <73415603+whoyak@users.noreply.github.com>
2024-12-16 21:51:50 +0300

    module_9_2.py

--- module_9_2/module_9_2.py
+++ module_9_2/module_9_2.py
@@ -1,18 +1,13 @@
 first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
 second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
 
+first_result = [len(length) for length in first_strings if len(length) >= 5]
 
-first_result = [len(length) for length in first_strings if len(length)>5]
+second_result = [(first, second) for first in first_strings for second in second_strings if len(first) == len(second)]
 
-second_result = [(first,second) for first in first_strings for second in second_strings if len(first) == len(second)]
-third = [*first_strings,*second_result]
-third_result = {thirds:len(thirds) for  thirds in third if len(thirds)%2 ==0}
-print(first_result)
-print(second_result)
-print(third_result)
+third = first_strings + second_strings  
+third_result = {string: len(string) for string in third if len(string) % 2 == 0}
 
-
-
-
-
-
+print(first_result)  
+print(second_result)  
+print(third_result)  
