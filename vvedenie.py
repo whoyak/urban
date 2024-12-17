dc9830ac53cda13bf976b608c9d681d3670f4ce8
whoyak <73415603+whoyak@users.noreply.github.com>
2024-12-17 21:44:35 +0300

    vvedenie.py

---
+++ vvedenie.py
@@ -0,0 +1,20 @@
+# 1st program
+print((9 ** 0.5) * 5)
+
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+expr1 = 2 * 2 + 2
+expr2 = 2 * (2 + 2)
+print(expr1)
+print(expr2)
+print(expr1 == expr2)
+
+# 4th program
+number = '123.456'
+float_number = float(number)
+shifted_number = float_number * 10
+whole_part = int(shifted_number)
+first_digit_after_point = whole_part % 10
+print(first_digit_after_point)
