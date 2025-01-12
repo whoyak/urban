fd34ac59d7ee17cf1c64a9983eba37d5b408c4e3
whoyak <73415603+whoyak@users.noreply.github.com>
2025-01-12 18:28:30 +0300

    tests_12_1.py

--- module_12_1/tests_12_1.py
+++ module_12_1/tests_12_1.py
@@ -1,43 +1,34 @@
 import unittest
-from Runner import Runner  # Замените на правильный путь к классу Runner
+from Runner import Runner  
 
 
 class RunnerTest(unittest.TestCase):
 
     def test_walk(self):
-        # Создаём объект Runner с произвольным именем
         runner = Runner("John")
 
-        # Вызываем метод walk 10 раз
         for _ in range(10):
             runner.walk()
 
-        # Проверяем, что distance после 10 вызовов метода walk равен 50
         self.assertEqual(runner.distance, 50)
 
     def test_run(self):
-        # Создаём объект Runner с произвольным именем
         runner = Runner("Jane")
 
-        # Вызываем метод run 10 раз
         for _ in range(10):
             runner.run()
-
-        # Проверяем, что distance после 10 вызовов метода run равен 100
         self.assertEqual(runner.distance, 100)
 
     def test_challenge(self):
-        # Создаём два объекта Runner с разными именами
+
         runner1 = Runner("John")
         runner2 = Runner("Jane")
 
-        # Вызываем методы run и walk 10 раз соответственно
         for _ in range(10):
             runner1.run()
         for _ in range(10):
             runner2.walk()
 
-        # Проверяем, что distance у объектов различаются
         self.assertNotEqual(runner1.distance, runner2.distance)
 
 
