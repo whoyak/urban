bba2fd8b8ccc25e3c2522738fb92669a6bd78c19
whoyak <73415603+whoyak@users.noreply.github.com>
2025-01-13 21:52:21 +0300

    Add files via upload

---
+++ module_13_3/module_13_3.py
@@ -0,0 +1,21 @@
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+
+logging.basicConfig(level=logging.INFO)
+
+API_TOKEN = "YOUR_BOT_API_TOKEN"
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=["start"])
+async def start(message: types.Message):
+    await message.answer("Привет! Я бот помогающий твоему здоровью.")
+
+@dp.message_handler()
+async def all_messages(message: types.Message):
+    await message.answer("Введите команду /start, чтобы начать общение.")
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
