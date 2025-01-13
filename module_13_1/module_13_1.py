00e3a3d9be9b416a511dc30e6567036e37ac79a6
whoyak <73415603+whoyak@users.noreply.github.com>
2025-01-13 21:41:46 +0300

    Add files via upload

---
+++ module_13_1/module_13_1.py
@@ -0,0 +1,22 @@
+import asyncio
+
+
+async def start_strongman(name, power):
+    print(f'Силач {name} начал соревнования')
+    for num in range(1,6):
+        await asyncio.sleep(1 / power)
+        print(f'Силач {name} поднял {num}')
+    print(f'Силач {name} закончил соревнования')
+
+
+async def start_tournament():
+    tasks = [
+        start_strongman('Pasha', 3),
+        start_strongman('Denis', 4),
+        start_strongman('Apollon', 5),
+    ]
+    await asyncio.gather(*tasks)
+
+
+if __name__ == "__main__":
+    asyncio.run(start_tournament())
